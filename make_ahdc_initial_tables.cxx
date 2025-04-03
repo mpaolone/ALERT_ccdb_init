79fcfd84314a1afd6346e8a29528f0cc3be81aad
Michael Paolone <mpaolone@gmail.com>
2025-04-03 15:25:53 -0400

    fixed typo in extra1 variable (was extra1r)

--- make_ahdc_initial_tables.cxx
+++ make_ahdc_initial_tables.cxx
@@ -12,7 +12,7 @@ void make_ahdc_initial_tables(){
 	int nrows = 0;
 	
 	ofstream ahdc_t0("AHDC_T0.txt");
-	ahdc_t0 << "# inital params (t0, dt0, extra1, extra2, chi2/ndf)" << endl;
+	ahdc_t0 << "# inital params (t0, dt0, extra1, extra2, chi2ndf)" << endl;
 	
 	for(int i = 0; i < nlays; i++){
 		for(int j = 0; j < ncomps[i]; j++){
@@ -34,7 +34,7 @@ void make_ahdc_initial_tables(){
 	ofstream ahdc_t2d("AHDC_T2D.txt");
 	
 	const int nparams2 = 13;
-	ahdc_t2d << "# inital params (p0, p1, p2, p3, p4, p5, dp0, dp1, dp2, dp3, dp4, dp5, chi2/ndf)" << endl;
+	ahdc_t2d << "# inital params (p0, p1, p2, p3, p4, p5, dp0, dp1, dp2, dp3, dp4, dp5, chi2ndf)" << endl;
 	
 	ahdc_t2d << "1 1 1";
 	for(int i = 0; i < nparams2; i++){
@@ -46,7 +46,7 @@ void make_ahdc_initial_tables(){
 	
 	TString curDir = gSystem->pwd();
 	
-	cout << "mktbl calibration/alert/ahdc/time_offsets -r 576 sector=int layer=int component=int t0=double dt0=double extra1r=double extra2=double chi2ndf=double #\'ALERT AHDC timing offset parameters\'" << endl;
+	cout << "mktbl calibration/alert/ahdc/time_offsets -r 576 sector=int layer=int component=int t0=double dt0=double extra1=double extra2=double chi2ndf=double #\'ALERT AHDC timing offset parameters\'" << endl;
 	cout << "add calibration/alert/ahdc/time_offsets " << curDir <<  "/AHDC_T0.txt" << endl;
 	cout << endl;
 	cout << "mktbl calibration/alert/ahdc/time_to_distance -r 1 sector=int layer=int component=int p0=double p1=double p2=double p3=double p4=double p5=double dp0=double dp1=double dp2=double dp3=double dp4=double dp5=double chi2ndf=double #\'ALERT AHDC time to distance parameters\'" << endl;
